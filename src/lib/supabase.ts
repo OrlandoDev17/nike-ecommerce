95801387e9f4669b6fddcaa40e87f609cbc65dc2
OrlandoDev17 <orlandodev1707@gmail.com>
2025-08-28 21:31:05 -0400

    Arreglar error de conexion con la base de datos

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,12 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseURL = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseURL || !supabaseKey) {
+  throw new Error(
+    "Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_ANON_KEY environment variables"
+  );
+}
 
 export const supabase = createClient(supabaseURL, supabaseKey);
